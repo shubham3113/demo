59fd2d013821d3051fc3d4e1369486ff65d806fe
Shubham Bangur <shubham.bangur@digivalet.com>
2024-03-27 12:36:54 +0530

    update sdk to 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.demojenkins"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.demojenkins"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
